74f8bac029ea9ce99f81eb83b83a3c256adf814b
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2021-02-02 10:53:33 +0000

    cool

---
+++ Game Engines 2 Examples 2020/Assets/Path.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+     public GameObject[] waypoints;
+    // Start is called before the first frame update
+    void Awake()
+    {
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+
+     public void OnDrawGizmos()
+    {
+        //foreach (GameObject way in waypoints)
+       // {
+           // Gizmos.DrawLine();
+       // }
+        Gizmos.color = Color.yellow;
+        foreach (GameObject go in waypoints)
+        Gizmos.DrawLine(transform.position, go.transform.position);
+
+         foreach (GameObject go in waypoints)
+         Gizmos.DrawWireSphere(go.transform.position, 1);
+     
+    }
+}
+
